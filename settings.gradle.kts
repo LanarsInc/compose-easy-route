eb5e377dde6174334993538f1ca314e986382f7e
Georgiy Strigun <georgiy.strigun@lanars.com>
2022-09-21 11:20:08 +0300

    Add repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        mavenLocal()
     }
 }
 dependencyResolutionManagement {
@@ -10,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
+        maven("https://jitpack.io")
     }
 }
 rootProject.name = "ComposeEasyRoute"
