8eb7eb728485b2bbeeee8decb7fc1e3f8eed66da
Georgiy Strigun <georgiy.strigun@lanars.com>
2022-09-21 18:31:17 +0300

    Add nav bar navigation sample

---
+++ sample/src/main/java/com/lanars/compose_easy_route/sample/pages/second/screens/ProfileScreen.kt
@@ -0,0 +1,23 @@
+package com.lanars.compose_easy_route.sample.pages.second.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.lanars.compose_easy_route.core.annotation.Destination
+import com.lanars.compose_easy_route.sample.pages.second.BottomNavigationNavGraph
+
+@BottomNavigationNavGraph
+@Destination("profile-page")
+@Composable
+fun ProfileScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Profile screen", fontSize = 32.sp)
+    }
+}
